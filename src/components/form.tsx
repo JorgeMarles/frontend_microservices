c7999b584fa8a1ed1baeef5e9f204528ed81eb2a
AlexaVJ <alexandra19vela@gmail.com>
2024-11-05 12:13:27 -0500

    Fix : Style of the component form

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -39,9 +39,9 @@ const Form = <T extends object>({
     return (
         <div className='bg-red-500 pb-5 pl-5 pr-5 m-5 rounded-2xl bg-opacity-70 backdrop-blur-md border-2 border-black'>
             <h1 className='text-8xl text-stroke font-Jomhuria'>{title}</h1>
-            <form onSubmit={handleSubmit} className='flex flex-col items-center'>
+            <form onSubmit={handleSubmit} className='flex flex-col items-center m-0'>
                 {Object.keys(fields).map(key => (
-                    <div key={key} className="flex items-stretch border border-black bg-white  w-64 m-2">
+                    <div key={key} className="flex items-stretch border border-black bg-white  w-64 mx-2 mb-2">
                         <div className='flex items-center justify-center p-2 border-r border-black'>
                             {fields[key].icon}
                         </div>
@@ -55,7 +55,7 @@ const Form = <T extends object>({
                         />
                     </div>
                 ))}
-                <button type="submit" className='rounded-full bg-white m-5 px-5 py-2 text-black hover:text-black hover:bg-gray-300 border border-black '>
+                <button type="submit" className='rounded-full bg-white m-1 px-5 py-2 text-black hover:text-black hover:bg-gray-300 border border-black '>
                     Submit
                 </button>
                 <div className='flex items-center'>
