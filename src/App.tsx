203746c931062f882cd7e8d84a19f6c8f4be9a82
AlexaVJ <alexandra19vela@gmail.com>
2024-11-06 12:06:08 -0500

    feat : Create the component problem and the page create problem

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,21 @@ import Footer from './components/Footer';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import UpdatePassword from './pages/UpdatePassword';
+import CreateProblem from './pages/problems/CreateProblem';
 
 const App: React.FC = () => {
 
-  
+
   return (
     <div className='h-screen w-screen flex flex-col'>
       <Header></Header>
       <div className='flex flex-grow'>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/forgot_password" element={<UpdatePassword />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/forgot_password" element={<UpdatePassword />} />
+          <Route path="/create_problem" element={<CreateProblem />} />
+        </Routes>
       </div>
       <Footer></Footer>
     </div>
---
+++ src/utils/problem_structure.tsx
@@ -0,0 +1,7 @@
+export interface Problem_structure {
+    name : string;
+    statement : string;
+    example_input : string;
+    example_output : string;
+    topic : string;    
+}
