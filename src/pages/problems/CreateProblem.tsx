c9f0a176ea38e0e39b449bc0ef2294fad87d9c80
Carlos Salazar <carlosalbertosame@ufps.edu.co>
2024-11-06 22:09:08 -0500

    The axios of problem was added

--- src/pages/problems/CreateProblem.tsx
+++ src/pages/problems/CreateProblem.tsx
@@ -2,12 +2,18 @@ import React, { FC } from 'react';
 import { Problem_structure } from '../../utils/problem_structure';
 import Problem from '../../components/Problem';
 import { field_problem } from '../../utils/field';
+import axios from 'axios';
 
 const CreateProblem: FC = () => {
 
-  const handleCreateProblem = (problem: Problem_structure) => {
+  const handleCreateProblem = async (problem: Problem_structure) => {
     console.log(problem);
-    
+    try {
+      const response = await axios.post("http://localhost:8080/problem", problem);
+    }
+    catch (error) {
+      console.error("Error submitting data:", error);
+    }
   }
 
 
--- src/utils/problem_structure.tsx
+++ src/utils/problem_structure.tsx
@@ -1,6 +1,8 @@
 export interface Problem_structure {
     name : string;
     statement : string;
+    input : string;
+    output : string;
     example_input : string;
     example_output : string;
     topic : string;    
