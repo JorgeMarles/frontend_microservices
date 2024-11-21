0f76c71f65fb75102e63e43fa2696c713ef449af
AlexaVJ <alexandra19vela@gmail.com>
2024-11-21 18:10:06 -0500

    Feat: Add navigate fuction in the menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,19 +1,32 @@
 import { FC } from 'react';
 import Search from './Search';
+import { NavLink } from 'react-router-dom';
 
 const Menu: FC = () => {
+
+    const menuItems = [
+        { label: "ProblemSet", path: "/home" },
+        { label: "Submissions", path: "/submissions" },
+        { label: "Ranking", path: "/ranking" },
+    ];
+    
     return (
         <nav className='m-5'>
             <ul className="flex flex-row items-center">
+            {menuItems.map((item) => ( 
                 <li className='px-8 border-r-4 border-gray-500 font-Jomhuria text-5xl'>
-                    <a href="#" className="" aria-current="page">ProblemSet</a>
+                    <NavLink
+                        to={item.path}
+                        className={({ isActive }) =>
+                            isActive
+                                ? "border-b-4 border-red-800"
+                                : "hover:text-red-800"
+                        }
+                    >
+                        {item.label}
+                    </NavLink>
                 </li>
-                <li className='px-8 border-r-4 border-gray-500 font-Jomhuria text-5xl'>
-                    <a href="#" className="" aria-current="page">Submissions</a>
-                </li>
-                <li className='px-8 border-r-4 border-gray-500 font-Jomhuria text-5xl'>
-                    <a href="#" className="" aria-current="page">Ranking</a>
-                </li>
+                ))}
                 <li className='px-8 text-xl'>
                     <Search />
                 </li>
