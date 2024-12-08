0956bbeaf38be2bee98ee63bb2f746b863dac965
AlexaVJ <alexandra19vela@gmail.com>
2024-12-08 18:01:24 -0500

    feat(navbar): add role-based differences to menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,33 +1,48 @@
 import { FC } from 'react';
 import Search from './Search';
 import { NavLink } from 'react-router-dom';
+import { jwtDecode } from 'jwt-decode';
+
+
+interface TokenPayload {
+    nickname: string;
+    type: string;
+    exp: number;
+}
+
 
 const Menu: FC = () => {
+    const token = sessionStorage.getItem('token');
+
+    const decodedToken = token ? jwtDecode<TokenPayload>(token) : { type: "user" } as TokenPayload;
 
     const menuItems = [
         { label: "ProblemSet", path: "/home" },
         { label: "Submissions", path: "/submissions" },
         { label: "Ranking", path: "/ranking" },
-        { label: "Create Problem", path: "/createProblem" },
+        { label: "Create Problem", path: "/createProblem", requiresAdmin: true },
     ];
 
     return (
         <nav className='m-5'>
             <ul className="flex flex-row items-center">
-                {menuItems.map((item, key) => (
-                    <li className='px-8 border-r-4 border-gray-500 font-Jomhuria text-5xl' key={key}>
-                        <NavLink
-                            to={item.path}
-                            className={({ isActive }) =>
-                                isActive
-                                    ? "border-b-4 border-red-800"
-                                    : "hover:text-red-800"
-                            }
-                        >
-                            {item.label}
-                        </NavLink>
-                    </li>
-                ))}
+                {menuItems.map((item, key) => {
+                    if (decodedToken.type !== "admin" && item.requiresAdmin) return null;
+                    return (
+                        <li className='px-8 border-r-4 border-gray-500 font-Jomhuria text-5xl' key={key}>
+                            <NavLink
+                                to={item.path}
+                                className={({ isActive }) =>
+                                    isActive
+                                        ? "border-b-4 border-red-800"
+                                        : "hover:text-red-800"
+                                }
+                            >
+                                {item.label}
+                            </NavLink>
+                        </li>
+                    );
+                })}
                 <li className='px-8 text-xl'>
                     <Search />
                 </li>
