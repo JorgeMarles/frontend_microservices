9655af377161a4c47e1c08702f202782e150544f
AlexaVJ <alexandra19vela@gmail.com>
2024-12-08 11:21:44 -0500

    feat(register): add error handling to user registration

--- src/fetch/UserFetch.tsx
+++ src/fetch/UserFetch.tsx
@@ -2,13 +2,12 @@ import axios from 'axios';
 import { User } from '../utils/interfaces';
 import { URL_BACKEND_USERS } from '../configs/config';
 
-export const Create = () => {
-    const createUser = async (userData : User) => {
-        try {
-            await axios.post(`${URL_BACKEND_USERS}/user`, userData);
-        } catch (error) {
-            console.error('Error creating user:', error);
-        }
+export const createUser = async (userData: User) => {
+    try {
+        const response = await axios.post(`${URL_BACKEND_USERS}/user`, userData);
+        return response; // Devuelve la respuesta si la solicitud tiene éxito
+    } catch (error) {
+        console.error('Error creating user:', error);
+        throw error; // Lanza el error para manejarlo desde el lugar donde se llama
     }
-    return { createUser };
-}
+};
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -4,25 +4,41 @@ import { fields_register } from '../utils/field';
 import Form from '../components/Form';
 import { backgroundURL } from '../assets/Images';
 import { useNavigate } from 'react-router-dom';
-import { Create } from '../fetch/UserFetch';
+import { createUser } from '../fetch/UserFetch';
 
 const Register: FC = () => {
   const navigate = useNavigate();
-  const { createUser } = Create();
 
-  const handleRegister = (user: User) => {
-    createUser(user);
-    navigate('/');
-  }
+  const handleRegister = async (user: User) => {
+    if (user.password !== user.repeat_password) {
+      alert("Passwords do not match. Please verify and try again.");
+      return;
+    }
 
+    try {
+      const response = await createUser(user);
+      if (response.status === 201) {
+        alert("User successfully created");
+        navigate('/');
+      } else {
+        alert(`Error creating user: ${response.statusText || 'Unknown error'}`);
+      }
+    } catch (error) {
+      console.error("Error creating user:", error);
+      alert("Error creating user");
+    }
+  };
 
   return (
-    <div className='flex items-center justify-center fullscreen-background'  style={{ backgroundImage: `url(${backgroundURL})` }}>
+    <div
+      className='flex items-center justify-center fullscreen-background'
+      style={{ backgroundImage: `url(${backgroundURL})` }}
+    >
       <Form<User>
         title='Register'
         fields={fields_register}
         onSubmit={handleRegister}
-        textSubmit ='Create account'
+        textSubmit='Create account'
         redirect={false}
         sendCode={false}
       />
