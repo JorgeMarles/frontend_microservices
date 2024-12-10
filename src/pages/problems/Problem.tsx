cac754eef44032c04d627160d4142c861a27e831
AlexaVJ <alexandra19vela@gmail.com>
2024-12-10 00:10:03 -0500

    feat: add status table for problems

--- src/pages/problems/Problem.tsx
+++ src/pages/problems/Problem.tsx
@@ -1,16 +1,19 @@
 import { FC, useEffect, useState } from 'react';
 import Menu from '../../components/Menu';
-import { useNavigate, useParams } from 'react-router-dom';
+import { NavLink, useNavigate, useParams } from 'react-router-dom';
 import ProblemView from '../../components/ProblemView';
 import { field_problem } from '../../utils/field';
-import { Problem as ProblemInterface } from '../../utils/interfaces';
+import { Problem as ProblemInterface, Submission } from '../../utils/interfaces';
 import { getByID } from '../../fetch/ProblemFetch';
 import { problem } from '../../utils/emptyEntities';
 import FileCard from '../../components/cards/FileCard';
 
+import submission from '../../data/submissionsOneProblem.json';
+
 const Problem: FC = () => {
     const { id } = useParams();
     const [data, setData] = useState<ProblemInterface>(problem);
+    const [submissions, setSubmissions] = useState<Submission[]>();
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -19,6 +22,7 @@ const Problem: FC = () => {
                 const idProblem = id !== undefined ? parseInt(id) : 0;
                 const response = await getByID(idProblem);
                 setData(response.problem);
+                setSubmissions(submission);
             }
             catch (error) {
                 console.error('Error fetching data: ', error);
@@ -55,6 +59,25 @@ const Problem: FC = () => {
                             type=".cpp"
                             share={true}
                         />
+                        <div className='py-5'>
+                            <div className='bg-red-700 w-full border border-black'>
+                                <h1 className='text-stroke font-Jomhuria text-6xl px-5 pt-2'>Status</h1>
+                            </div>
+                            {submissions && submission.map((item, key) =>
+                                <div className='grid grid-cols-2 border-l border-r border-b border-black' key={key}>
+                                    <NavLink
+                                        to={`/submission/${item.id}`}
+                                        className="text-blue-800 text-center border-r border-black"
+                                    >
+                                        {item.id}
+                                    </NavLink>
+                                    <div className="text-center text-lg capitalize">
+                                        {item.veredict}
+                                    </div>
+                                </div>
+                            )}
+
+                        </div>
                     </div>
                 </div>
             </div>
