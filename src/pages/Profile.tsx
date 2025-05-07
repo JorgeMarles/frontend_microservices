05efa52c582bedde3dde6bbb5ab7819370134185
Juan Afanador <juanafanador07@gmail.com>
2025-05-07 14:12:33 -0500

    chore: Remove unused vars

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,15 +1,14 @@
 import { FC, useEffect, useState } from "react";
-import { NavLink, useParams } from "react-router-dom";
-import { ContestPosition, Submission, User } from "../utils/interfaces";
+import { useParams } from "react-router-dom";
+import { ContestPosition, User } from "../utils/interfaces";
 import { user as emptyUser } from "../utils/emptyEntities";
 import { getUser, updateUser } from "../fetch/UserFetch";
 import Menu from "../components/Menu";
-import { getEmailUser, getTypeUser } from "../session/Token";
+import { getEmailUser } from "../session/Token";
 import FormUser from "../components/forms/FormUser";
 import UserCard from "../components/cards/UserCard";
 
 import stadisticsJSON from "../data/stadistics.json";
-import { getAllByUser } from "../fetch/SubmissionFetch";
 import { getContestPositions } from "../fetch/ContestFetch";
 import { Line } from "react-chartjs-2";
 import "chart.js/auto";
@@ -85,59 +84,15 @@ const process = async (
   };
 };
 
-const processSubmissions = (data: Submission[]) => {
-  const veredicts: Veredict[] = [
-    {
-      label: "Accepted",
-      type: "Accepted",
-      value: 0,
-    },
-    {
-      label: "Wrong answer",
-      type: "Wrong answer",
-      value: 0,
-    },
-    {
-      label: "Time limited exceded",
-      type: "Time Limit Exceeded",
-      value: 0,
-    },
-    {
-      label: "Runtime error",
-      type: "Runtime error",
-      value: 0,
-    },
-    {
-      label: "Compilation error",
-      type: "Compilation error",
-      value: 0,
-    },
-  ];
-  let total = 0;
-  for (const item of data) {
-    for (const veredict of veredicts) {
-      if (item.veredict === veredict.type) {
-        veredict.value++;
-        total++;
-      }
-    }
-  }
-  return { veredicts, total };
-};
-
 const Profile: FC = () => {
   const { id } = useParams();
   const [data, setData] = useState<UserInfo>(emptyUser);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [edit, setEdit] = useState(false);
   const [action, setAction] = useState(0);
-  const [veredicts, setVeredicts] = useState<Veredict[]>();
-  const [topics, setTopics] = useState<TopicStadistic[]>();
-  const [auxiliar, setAuxiliar] = useState(0);
   const [contestPositions, setContestPositions] = useState<ContestPosition[]>(
     []
   );
-  const type = getTypeUser();
 
   useEffect(() => {
     const fetchUser = async (email?: string, idUser?: number) => {
@@ -145,13 +100,7 @@ const Profile: FC = () => {
         setIsLoading(true);
         const response = await getUser(email, idUser);
         // const responseProblem = await getProblemsInfo(response.data.user.id);
-        const responseSubmissions = await getAllByUser(response.data.user.id);
-        const submissions: Submission[] = responseSubmissions?.data;
         const data = await process(stadisticsJSON.topics);
-        const results = processSubmissions(submissions);
-        setVeredicts(results.veredicts);
-        setAuxiliar(results.total);
-        setTopics(data.topics);
         const userInfo = {
           ...response.data.user,
           totalProblems: data.total,
